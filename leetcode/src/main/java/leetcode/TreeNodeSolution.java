6ea5398642da9a5a80d1b0310b7babfa6448e791
cj555 <chengjuan555@gmail.com>
2016-03-18 20:16:35 +0800

    blog-aggregator-init

---
+++ leetcode/src/main/java/leetcode/TreeNodeSolution.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+
+class TreeNode {
+
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class TreeNodeSolution {
+
+	/**
+	 * Definition for a binary tree node.
+	 */
+	int sum = 0;
+
+	public int rob(TreeNode root) {
+
+		root.val 
+
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		TreeNode tn = new TreeNode(3);
+
+		for (int i = 0; i < 10; i++) {
+
+			
+			tn.right = new TreeNode(i+1);
+
+		}
+
+		return tn;
+		
+		
+	}
+
+}
