cd95a9189fa8da6d99dcc1d7fc96823f54e3f8c2
cj555 <chengjuan555@gmail.com>
2016-03-19 09:27:21 +0800

    update

--- blog-aggregator/src/main/java/io/cj555/entity/Item.java
+++ blog-aggregator/src/main/java/io/cj555/entity/Item.java
@@ -3,10 +3,16 @@ package io.cj555.entity;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Item {
-
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
 	private String title;
 	private String description;
 	
