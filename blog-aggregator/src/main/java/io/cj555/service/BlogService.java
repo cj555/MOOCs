84d5700ada4229b262310be32a3a4c6eba2d3ec5
cj555 <chengjuan555@gmail.com>
2016-03-23 13:48:17 +0800

    schedule item saveing for every one hour

--- blog-aggregator/src/main/java/io/cj555/service/BlogService.java
+++ blog-aggregator/src/main/java/io/cj555/service/BlogService.java
@@ -3,6 +3,7 @@ package io.cj555.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.access.method.P;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
@@ -30,12 +31,22 @@ public class BlogService {
 	@Autowired
 	private ItemRepository itemRepository;
 
+	// 1hour
+	@Scheduled(fixedDelay = 3600000)
+	public void reloadBlogs() {
+		List<Blog> blogs = blogRepository.findAll();
+		for (Blog blog : blogs) {
+			saveItems(blog);
+		}
+
+	}
+
 	public void saveItems(Blog blog) {
 		try {
 			List<Item> items = rssService.getItems(blog.getUrl());
 			for (Item item : items) {
 				Item savedItem = itemRepository.findByBlogAndLink(blog, item.getLink());
-				if(savedItem == null) {
+				if (savedItem == null) {
 					item.setBlog(blog);
 					itemRepository.save(item);
 				}
